a72d1af4b3b2162c6bbdd80d9b8bbedf43459569
Dmytro Tolok <tolok.d@gmail.com>
2023-11-07 17:06:37 +0100

    feat(ts): add ts and couple of other stuff

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import 'dotenv/config';
+import { remDB, locDB } from './mysql';
---
+++ src/mysql.ts
@@ -0,0 +1,36 @@
+import mysql from 'mysql2/promise';
+import { ConnectionString } from 'connection-string';
+export { OkPacket, FieldPacket } from 'mysql2/promise';
+
+const { protocol: remProtocol, hosts: remHosts, path: remPath, ...remConfig } = new ConnectionString(process.env.DATABASE_URL || '');
+const remDB: mysql.Pool = mysql.createPool({
+  ...remConfig,
+  host: remHosts?.[0].name,
+  port: remHosts?.[0].port,
+  connectionLimit: parseInt(process.env.CONNECTION_LIMIT || '10'),
+  multipleStatements: true,
+  connectTimeout: 60e3,
+  waitForConnections: true,
+  charset: 'utf8mb4',
+  database: remPath?.[0],
+  queueLimit: 0
+});
+
+const { protocol: locProtocol, hosts: locHosts, path: locPath, ...locConfig } = new ConnectionString(process.env.LOCAL_DATABASE_URL || '');
+const locDB: mysql.Pool = mysql.createPool({
+  ...locConfig,
+  host: locHosts?.[0].name,
+  port: locHosts?.[0].port,
+  connectionLimit: parseInt(process.env.CONNECTION_LIMIT || '10'),
+  multipleStatements: true,
+  connectTimeout: 60e3,
+  waitForConnections: true,
+  charset: 'utf8mb4',
+  database: locPath?.[0],
+  queueLimit: 0
+});
+
+export {
+  remDB,
+  locDB
+}
